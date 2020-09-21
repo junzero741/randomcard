fa2faeffdb4a17b2b9f057c8e39faaef3dc1668e
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-09-21 12:52:26 +0900

    trump card random picker

---
+++ RandomArray.java
@@ -0,0 +1,20 @@
+
+/*
+ * public class RandomArray {
+ * 
+ * public static int[] shuffle(int[] arr) {
+ * 
+ * public static void main(String[] args) {
+ * 
+ * 
+ * int[] arr = {1,2,3,4,5,6,7,8,9};
+ * System.out.println(java.util.Arrays.toString(arr));
+ * 
+ * int[] arr_random = shuffle(arr);
+ * System.out.println(java.util.Arrays.toString(arr_random));
+ * 
+ * 
+ * }
+ * 
+ * }
+ */
---
+++ RandomCardApp.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class RandomCardApp {
+
+	public static void main(String[] args) {
+		
+		
+		String[] num = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		String[] shape = {"heart", "spade", "diamond", "clover"};
+//		String[][] cards = {{"2", "heart"},{"2", "spade"}};
+		
+		
+//		var randomNum =  (int) Math.ceil(Math.random()*10) + 2;// random between 5~12 --- > 0~12
+		
+		System.out.println(num[getRandomNumberInRange(0, 12)]); // number random 0~12
+		System.out.println(shape[getRandomNumberInRange(0, 3)]); // shape random heart ~ clover
+		
+	}
+	private static int getRandomNumberInRange(int min, int max) {
+
+		if (min >= max) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+
+		Random r = new Random();
+		return r.nextInt((max - min) + 1) + min;
+		
+	}
+	
+}
